4db45b0bffb9a4f0a630aa03aab2979f50e4d7aa
Vikas S Biradar <164601893+VikasSBiradar@users.noreply.github.com>
2024-10-14 14:16:23 +0530

    Day 22 challenge

---
+++ 1873_calculate_special_bonus(Day_22).sql
@@ -0,0 +1,7 @@
+/* Write your T-SQL query statement below */
+SELECT employee_id, 
+CASE WHEN employee_id % 2 = 1 AND name NOT LIKE 'M%'
+THEN salary
+ELSE 0 END AS bonus
+FROM Employees
+ORDER BY employee_id
