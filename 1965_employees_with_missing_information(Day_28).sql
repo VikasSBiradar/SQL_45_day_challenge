8a527b1c6562d86be4c2fc0d164235fdd3e6aa62
Vikas S Biradar <164601893+VikasSBiradar@users.noreply.github.com>
2024-10-22 13:24:52 +0530

    Day 28 challenge

---
+++ 1965_employees_with_missing_information(Day_28).sql
@@ -0,0 +1,5 @@
+/* Write your T-SQL query statement below */
+SELECT employee_id FROM Employees WHERE employee_id NOT IN (SELECT employee_id  FROM Salaries) 
+UNION
+SELECT employee_id FROM Salaries WHERE employee_id NOT IN (SELECT employee_id  FROM Employees) 
+ORDER BY employee_id
